db08f646afa0aede1d799b888b1355433edf0236
Matija Petrunic <matija.petrunic@gmail.com>
2020-05-21 14:50:03 +0200

    Remove empty files

--- packages/example/src/services/format.ts
+++
@@ -1,7 +0,0 @@
-export function shortAddress(address: string): string {
-    return address.slice(0, 7) + "....." + address.slice(-7);
-}
-
-export function getCurrency(network: string): string {
-    return "";
-}
