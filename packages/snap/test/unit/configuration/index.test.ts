061519480b9fb0b249310c36f0c4253b72e24d39
Matija Petrunic <matija.petrunic@gmail.com>
2020-05-21 14:23:36 +0200

    Remove missing import

--- packages/snap/test/unit/configuration/index.test.ts
+++ packages/snap/test/unit/configuration/index.test.ts
@@ -1,8 +1,5 @@
 import chai, {expect} from "chai";
 import sinonChai from "sinon-chai";
-import {WalletMock} from "../wallet.mock.test";
-import {EmptyMetamaskState} from "../../../src/interfaces";
-import {SnapConfig} from "@nodefactory/metamask-filecoin-types";
 
 chai.use(sinonChai);
 
