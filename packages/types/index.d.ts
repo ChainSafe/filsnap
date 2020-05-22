2a079e58e27a68f54950618512d30218e0cc787a
Matija Petrunic <matija.petrunic@gmail.com>
2020-05-22 09:18:30 +0200

    Remove rpc method implementations

--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -1,98 +1,4 @@
-export interface GetPublicKeyRequest{
-  method: "getPublicKey";
-}
-
-export interface GetAddressRequest {
-  method: "getAddress";
-}
-
-export interface ExportSeedRequest {
-  method: "exportSeed";
-}
-
-export interface GetTransactionsRequest {
-  method: "getAllTransactions";
-  params: {
-    address?: string;
-  };
-}
-
-export interface GetBlockRequest {
-  method: "getBlock";
-  params: {
-    blockTag?: BlockId;
-  };
-}
-
-export interface GetBalanceRequest {
-  method: "getBalance";
-}
-
-
-export interface ConfigureSnapRequest {
-  method: "configure";
-  params: {
-    configuration: SnapConfig;
-  };
-}
-
-export interface AddFilecoinAssetRequest {
-  method: "addFilecoinasset";
-}
-
-export interface RemoveFilecoinAssetRequest {
-  method: "removeFilecoinAsset";
-}
-
-export interface GetChainHeadRequest {
-  method: "getChainHead";
-}
-
-export interface SignPayloadJSONRequest {
-  method: "signPayloadJSON";
-  params: {
-    payload: any;
-  };
-}
-
-export interface SignPayloadRawRequest {
-  method: "signPayloadRaw";
-  params: {
-    payload: any;
-  };
-}
-
-export interface GenerateTransactionPayload {
-  method: "generateTransactionPayload";
-  params: {
-    amount: string|number;
-    to: string;
-  };
-}
-
-export interface SendUnitRequest {
-  method: "send";
-  params: {
-    signature: string;
-    txPayload: TxPayload;
-  };
-}
-
-export type MetamaskFilecoinRpcRequest =
-    GetPublicKeyRequest
-    | GetAddressRequest
-    | ExportSeedRequest
-    | GetTransactionsRequest
-    | GetBlockRequest
-    | GetBalanceRequest
-    | ConfigureSnapRequest
-    | AddFilecoinAssetRequest
-    | RemoveFilecoinAssetRequest
-    | GetChainHeadRequest
-    | SignPayloadJSONRequest
-    | SignPayloadRawRequest
-    | SendUnitRequest
-    | GenerateTransactionPayload;
+export type MetamaskFilecoinRpcRequest = any;
 
 type Method = MetamaskFilecoinRpcRequest["method"];
 
@@ -133,10 +39,6 @@ export interface UnitConfiguration {
 }
 
 export interface SnapConfig {
-  networkName: string;
-  wsRpcUrl?: string;
-  addressPrefix?: number;
-  unit?: UnitConfiguration;
 }
 
 // Filecoin types
