6caff08dbe1453e17a1d5553266ce9d68591e70b
Matija Petrunic <matija.petrunic@gmail.com>
2020-05-25 16:16:27 +0200

    Declare wasm module types

--- packages/snap/src/filecoin/account.ts
+++ packages/snap/src/filecoin/account.ts
@@ -1,8 +1,6 @@
-
 import {Wallet} from "../interfaces";
 import {getConfiguration} from "../configuration/index"
-import {ExtendedKey} from "@nodefactory/metamask-filecoin-types";
-import * as signer_wasm from "@zondax/filecoin-signer-wasm";
+import {key_derive_from_seed, ExtendedKey} from "@zondax/filecoin-signer-wasm";
 
 /**
  * Return derived KeyPair from seed.
@@ -12,6 +10,6 @@ export async function getKeyPair(wallet: Wallet): Promise<ExtendedKey> {
   const seed = await wallet.getAppKey()
   const bip44 = getConfiguration(wallet).bip44
 
-  const keyPair: ExtendedKey = signer_wasm.key_derive_from_seed(seed, bip44);
+  const keyPair: ExtendedKey = key_derive_from_seed(seed, bip44);
   return keyPair;
 }
