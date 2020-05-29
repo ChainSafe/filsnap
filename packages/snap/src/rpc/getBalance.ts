4effdfd3a445835ee359e2fb7cc8acf3deb7f2fe
Matija Petrunic <matija.petrunic@gmail.com>
2020-05-29 12:11:54 +0200

    Implement get balance rpc method

---
+++ packages/snap/src/rpc/getBalance.ts
@@ -0,0 +1,16 @@
+import {Wallet} from "../interfaces";
+import {getKeyPair} from "../filecoin/account";
+import LotusRpcEngine from "@openworklabs/lotus-jsonrpc-engine";
+
+/**
+ * Returns balance as string
+ * @param wallet
+ * @param address
+ */
+export async function getBalance(wallet: Wallet, api: LotusRpcEngine, address?: string): Promise<string> {
+  if(!address) {
+    address = (await getKeyPair(wallet)).address;
+  }
+
+  return await api.request('WalletBalance', address) as string;
+}
---
+++ packages/snap/test/unit/rpc/getBalance.test.ts
@@ -0,0 +1,30 @@
+import chai, {expect} from "chai";
+import sinonChai from "sinon-chai";
+import {WalletMock} from "../wallet.mock.test";
+import {getBalance} from "../../../src/rpc/getBalance";
+import sinon from "sinon";
+import {testAppKey} from "./keyPairTestConstants";
+import {EmptyMetamaskState} from "../../../src/interfaces";
+
+chai.use(sinonChai);
+
+describe('Test rpc handler function: getBalance', function() {
+
+  const walletStub = new WalletMock();
+
+  afterEach(function() {
+    walletStub.reset();
+  });
+
+  it('should return balance on saved keyring in state', async function () {
+    walletStub.getAppKey.returns(testAppKey);
+    walletStub.getPluginState.returns(EmptyMetamaskState());
+    const apiStub = {request: sinon.stub()};
+    apiStub.request.returns("3");
+
+
+    const result = await getBalance(walletStub, apiStub);
+
+    expect(result).to.be.eq("3");
+  });
+});
