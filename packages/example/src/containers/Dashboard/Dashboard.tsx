a14682c31a83d70125edb499c8a59a5f366ecbbd
Mak Muftic <mak@nodefactory.io>
2020-06-15 15:23:56 +0200

    Add network selection to Dashboard

--- packages/example/src/containers/Dashboard/Dashboard.tsx
+++ packages/example/src/containers/Dashboard/Dashboard.tsx
@@ -1,7 +1,8 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, {useCallback, useContext, useEffect, useState} from "react";
 import {
+    Box,
     Container,
-    Grid, Hidden,
+    Grid, Hidden, InputLabel, MenuItem, Select,
 } from '@material-ui/core/';
 import {MetaMaskConnector} from "../MetaMaskConnector/MetaMaskConnector";
 import {MetaMaskContext} from "../../context/metamask";
@@ -16,12 +17,27 @@ export const Dashboard = () => {
     const [address, setAddress] = useState("");
     const [publicKey, setPublicKey] = useState("");
 
+    const [network, setNetwork] = useState<"f" | "t">("t");
+
     const [api, setApi] = useState<FilecoinSnapApi|null>(null);
+
+    const handleNetworkChange = async (event: React.ChangeEvent<{value: any}>) => {
+        const selectedNetwork = event.target.value as "f" | "t";
+        console.log("NETWORK");
+        console.log(selectedNetwork);
+        if (selectedNetwork === network) return;
+        // todo set initial configuration
+        if (api) {
+            // todo implement configuraitons with only network name
+            // api.configure({})
+        }
+    };
 
     useEffect(() => {
         (async () => {
             if (state.filecoinSnap.isInstalled && state.filecoinSnap.snap) {
                 const filecoinApi = await state.filecoinSnap.snap.getFilecoinSnapApi();
+                filecoinApi.configure({network: network})
                 setApi(filecoinApi);
             }
         })();
@@ -43,6 +59,16 @@ export const Dashboard = () => {
                     <MetaMaskConnector/>
                 </Hidden>
                 <Hidden xsUp={!state.filecoinSnap.isInstalled}>
+                    <Box m="1rem" alignSelf="baseline">
+                        <InputLabel>Network</InputLabel>
+                        <Select
+                            defaultValue={"t"}
+                            onChange={handleNetworkChange}
+                        >
+                            <MenuItem value={"t"}>Testnet</MenuItem>
+                            <MenuItem value={"f"}>Mainnet</MenuItem>
+                        </Select>
+                    </Box>
                     <Grid container spacing={3} alignItems="stretch">
                         <Grid item xs={12}>
                             <Account address={address} balance={"0"} publicKey={publicKey}/>
