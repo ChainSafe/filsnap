d757f2520c21fbc4fce81ebd351a568d7abb9ac8
Mak Muftic <mak@nodefactory.io>
2020-07-16 15:03:30 +0200

    Fix linter errors

--- packages/snap/src/filecoin/account.ts
+++ packages/snap/src/filecoin/account.ts
@@ -1,12 +1,14 @@
 import {Wallet} from "../interfaces";
+// eslint-disable-next-line
 // @ts-ignore
 import {keyDeriveFromSeed} from "@zondax/filecoin-signing-tools/js";
+import {KeyPair} from "@nodefactory/metamask-filecoin-types";
 
 /**
  * Return derived KeyPair from seed.
  * @param wallet
  */
-export async function getKeyPair(wallet: Wallet): Promise<any> {
+export async function getKeyPair(wallet: Wallet): Promise<KeyPair> {
   const seed = await wallet.getAppKey();
   const pluginState = await wallet.getPluginState();
   const extendedKey = keyDeriveFromSeed(seed, pluginState.filecoin.config.derivationPath);
