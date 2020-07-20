ec1090bcf8450b2b45931ccb4d5c385810706e58
Mak Muftic <mak@nodefactory.io>
2020-07-20 12:05:52 +0200

    Refactor sign message confirmation message

--- packages/snap/src/rpc/signMessage.ts
+++ packages/snap/src/rpc/signMessage.ts
@@ -1,14 +1,18 @@
-import {Message, SignedMessage, transactionSerialize, transactionSign} from "@zondax/filecoin-signing-tools/js";
+import {Message, SignedMessage, transactionSign} from "@zondax/filecoin-signing-tools/js";
 import {Wallet} from "../interfaces";
 import {getKeyPair} from "../filecoin/account";
 import {showConfirmationDialog} from "../util/confirmation";
 
 export async function signMessage(wallet: Wallet, message: Message): Promise<SignedMessage> {
   const keypair = await getKeyPair(wallet);
-  const tx = transactionSerialize(message);
   const confirmation = await showConfirmationDialog(
     wallet,
-    `Do you want to sign message ${tx} with account ${keypair.address}?`
+    `Do you want to sign message\n\n` +
+    `from: ${message.from}\n`+
+    `to: ${message.to}\n`+
+    `value:${message.value}\n`+
+    `gas price:${message.gasprice}\n\n` +
+    `with account ${keypair.address}?`
   );
   if (confirmation) {
     return transactionSign(message, keypair.privateKey);
