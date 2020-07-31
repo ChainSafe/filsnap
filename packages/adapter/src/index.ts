97f820f91732c9e84593e81689f9c19d1fbbe499
Mak Muftic <mak@nodefactory.io>
2020-07-31 15:00:45 +0200

    Add basic documentation to enableFilecoinSnap adapter function

--- packages/adapter/src/index.ts
+++ packages/adapter/src/index.ts
@@ -9,6 +9,19 @@ export type MetamaskFilecoinSnap = MFSnap;
 
 export {hasMetaMask, isMetamaskSnapsSupported} from "./utils";
 
+/**
+ * Install and enable Filecoin snap
+ *
+ * Checks for existence of Metamask and version compatibility with snaps before installation.
+ *
+ * Provided snap configuration must define at least network property so predefined configuration can be selected.
+ * All other properties are optional, and if present will overwrite predefined property.
+ *
+ * @param config - SnapConfig
+ * @param pluginOrigin
+ *
+ * @return MetamaskFilecoinSnap - adapter object that exposes snap API
+ */
 export async function enableFilecoinSnap(
   config: Partial<SnapConfig>, pluginOrigin?: string
 ): Promise<MetamaskFilecoinSnap> {
