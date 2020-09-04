7bc547e4512925e482eea995b345079661337dbb
kumavis <aaron@kumavis.me>
2020-09-04 16:34:05 +0800

    lint fix

--- packages/snap/src/filecoin/account.ts
+++ packages/snap/src/filecoin/account.ts
@@ -12,7 +12,7 @@ export async function getKeyPair(wallet: Wallet): Promise<KeyPair> {
   const pluginState = await wallet.getPluginState();
   const { derivationPath } = pluginState.filecoin.config;
   const bip44Code = derivationPath.split('/')[2].split('\'')[0];
-  const isTestnet = bip44Code !== '461'
+  const isTestnet = bip44Code !== '461';
   const rawBip44Entropy = await wallet.send({ method: `wallet_getBip44Entropy_${bip44Code}`, params: [] });
   const bip44Entropy = Buffer.from(String(rawBip44Entropy), 'base64');
   // metamask has supplied us with entropy for "m/purpose'/bip44Code'/"
