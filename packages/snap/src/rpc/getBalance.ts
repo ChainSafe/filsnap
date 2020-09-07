6c5cb8569067b4492b64230d8e38e9db098e3674
Mak Muftic <mak@nodefactory.io>
2020-09-07 16:07:33 +0200

    Format getBalance result to FIL

--- packages/snap/src/rpc/getBalance.ts
+++ packages/snap/src/rpc/getBalance.ts
@@ -1,10 +1,12 @@
 import {Wallet} from "../interfaces";
 import {getKeyPair} from "../filecoin/account";
 import {LotusRpcApi} from "../filecoin/types";
+import {FilecoinNumber} from '@openworklabs/filecoin-number';
 
 export async function getBalance(wallet: Wallet, api: LotusRpcApi, address?: string): Promise<string> {
   if(!address) {
     address = (await getKeyPair(wallet)).address;
   }
-  return await api.walletBalance(address);
+  const balance = await api.walletBalance(address);
+  return (new FilecoinNumber(balance, 'attofil')).toFil();
 }
