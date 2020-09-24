1c32a24ef5e68b61e65005e06302fedf5ae7d880
Mak Muftic <mak@nodefactory.io>
2020-09-24 16:52:56 +0200

    Expand getBalance method

--- packages/snap/src/rpc/getBalance.ts
+++ packages/snap/src/rpc/getBalance.ts
@@ -2,11 +2,18 @@ import {Wallet} from "../interfaces";
 import {getKeyPair} from "../filecoin/account";
 import {LotusRpcApi} from "../filecoin/types";
 import {FilecoinNumber} from '@openworklabs/filecoin-number';
+import {FilecoinNumberType} from "@nodefactory/filsnap-types";
 
-export async function getBalance(wallet: Wallet, api: LotusRpcApi, address?: string): Promise<string> {
-  if(!address) {
-    address = (await getKeyPair(wallet)).address;
-  }
+export async function getBalance(
+    wallet: Wallet, api: LotusRpcApi, currency: FilecoinNumberType = "fill"
+): Promise<string> {
+  const address = (await getKeyPair(wallet)).address;
+  return await getBalanceForAddress(wallet, api, currency, address);
+}
+
+export async function getBalanceForAddress(
+    wallet: Wallet, api: LotusRpcApi, currency: FilecoinNumberType = "fill", address: string
+): Promise<string> {
   const balance = await api.walletBalance(address);
-  return (new FilecoinNumber(balance, 'attofil')).toFil();
+  return (new FilecoinNumber(balance, currency)).toFil();
 }
