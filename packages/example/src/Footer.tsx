74c898b0ded3e6fbfb768f9b01775b19f4b5a948
Marin <marin.petrunic@gmail.com>
2021-08-31 14:11:43 +0200

    Fix repo ref link in example

--- packages/example/src/Footer.tsx
+++ packages/example/src/Footer.tsx
@@ -10,7 +10,7 @@ function Footer() {
         <Container style={{position: "fixed", left: 0, bottom: 0, maxWidth: "100%", textAlign: "center"}}>
             <Grid style={{}} direction="row" alignItems="flex-end" justify="center" container spacing={3}>
                 <Box m="2rem" height="100%">
-                    <a style={{textDecoration: "unset"}} href={"https://github.com/NodeFactoryIo/filsnap"}>
+                    <a style={{textDecoration: "unset"}} href={"https://github.com/Metamask/filsnap"}>
                         <GitHubIcon fontSize={"large"} />
                         <Typography style={{textDecoration: "unset"}}>Repo</Typography>
                     </a>
