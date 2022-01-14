fe438043dd9af82d88faa6c0a21a5738e5e23c3a
Mak Muftic <mak@chainsafe.io>
2022-01-14 13:14:47 +0100

    Add max fee

--- packages/snap/src/rpc/estimateMessageGas.ts
+++ packages/snap/src/rpc/estimateMessageGas.ts
@@ -2,6 +2,7 @@ import {Wallet} from "../interfaces";
 import {Message, MessageGasEstimate, MessageRequest} from "@chainsafe/filsnap-types";
 import {getKeyPair} from "../filecoin/account";
 import {LotusRpcApi} from "../filecoin/types";
+import { FilecoinNumber } from "@openworklabs/filecoin-number";
 
 export async function estimateMessageGas(
   wallet: Wallet, api: LotusRpcApi, messageRequest: MessageRequest
@@ -16,8 +17,9 @@ export async function estimateMessageGas(
     method: 0, // code for basic transaction
     nonce: 0 // dummy nonce just for gas calculation
   };
-    // estimate gas usage
+  // estimate gas usage
   const gasLimit = await api.gasEstimateGasLimit(message, null);
-  const messageEstimate = await api.gasEstimateMessageGas(message, {MaxFee: "0"}, null);
+  const maxFee = new FilecoinNumber('0.1', "fill").toAttoFil(); // set max fee to 0.1 FIL
+  const messageEstimate = await api.gasEstimateMessageGas(message, {MaxFee: maxFee}, null);
   return {gasfeecap: messageEstimate.GasFeeCap, gaslimit: gasLimit, gaspremium: messageEstimate.GasPremium};
 }
