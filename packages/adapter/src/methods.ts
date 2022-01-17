92f23d8e4ff7e0dca6f3c3e7cd0efe8cd36caa4a
Mak Muftic <mak@chainsafe.io>
2022-01-17 14:51:10 +0100

    Fix linter errors

--- packages/adapter/src/methods.ts
+++ packages/adapter/src/methods.ts
@@ -55,5 +55,8 @@ export async function getMessages(this: MetamaskFilecoinSnap): Promise<MessageSt
 export async function calculateGasForMessage(
   this: MetamaskFilecoinSnap, message: MessageRequest, maxFee?: string
 ): Promise<MessageGasEstimate> {
-  return await sendSnapMethod({method: "fil_getGasForMessage", params: {message: message, maxFee: maxFee}}, this.snapId);
+  return await sendSnapMethod(
+    {method: "fil_getGasForMessage", params: {maxFee: maxFee, message: message}}, 
+    this.snapId,
+  );
 }
