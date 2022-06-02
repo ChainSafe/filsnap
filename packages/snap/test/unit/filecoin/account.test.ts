39f5385fb34a0bb34e4a082941e79817efcf6c7a
Bernard <bero4net@gmail.com>
2022-06-02 17:05:27 +0200

    improve getKeyPair tests

--- packages/snap/test/unit/filecoin/account.test.ts
+++ packages/snap/test/unit/filecoin/account.test.ts
@@ -2,7 +2,13 @@ import chai, {expect} from "chai";
 import sinonChai from "sinon-chai";
 import {WalletMock} from "../wallet.mock.test";
 import {getKeyPair} from "../../../src/filecoin/account";
-import {testAddress, testBip44Entropy, testPrivateKeyBase64, testPublicKey} from "../rpc/keyPairTestConstants";
+import {
+  testAddress,
+  testNewBip44Entropy, testNewMetamaskVersion, testOldBip44Entropy,
+  testOldMetamaskVersion,
+  testPrivateKeyBase64,
+  testPublicKey
+} from "../rpc/keyPairTestConstants";
 import {SnapConfig} from "@chainsafe/filsnap-types";
 
 chai.use(sinonChai);
@@ -15,18 +21,41 @@ describe('Test account function: getKeyPair', function() {
     walletStub.reset();
   });
 
-  it('should return valid keypair for filecoin mainnnet', async function() {
+  it('should return valid keypair for filecoin mainnnet with old version of metamask', async function() {
     walletStub.rpcStubs.snap_manageState
       .withArgs('get')
-      .resolves({filecoin: {config: {network: "f", derivationPath: "m/44'/461'/0'/0/0"} as SnapConfig}});
-    walletStub.rpcStubs.snap_getBip44Entropy_461.resolves(testBip44Entropy);
+      .resolves({filecoin: {config: {derivationPath: "m/44'/461'/0'/0/0", network: "f"} as SnapConfig}});
+
+    walletStub.rpcStubs.snap_getBip44Entropy_461.resolves(testOldBip44Entropy);
+    walletStub.rpcStubs.web3_clientVersion.resolves(testOldMetamaskVersion);
     // ensure our call to getBip44Entropy returns the correct entropy
-    walletStub.requestStub.resolves(testBip44Entropy);
+    walletStub.requestStub.resolves(testOldBip44Entropy);
+    const result = await getKeyPair(walletStub);
+
+    expect(result.publicKey).to.be.eq(testPublicKey);
+    expect(result.address).to.be.eq(testAddress);
+    expect(result.privateKey).to.be.eq(testPrivateKeyBase64);
+    expect(walletStub.rpcStubs.snap_getBip44Entropy_461).to.have.been.calledOnce;
+    expect(walletStub.rpcStubs.snap_manageState).to.have.been.calledOnce;
+    expect(walletStub.rpcStubs.web3_clientVersion).to.have.been.calledOnce;
+  });
+
+  it('should return valid keypair for filecoin mainnnet with new version of metamask', async function() {
+    walletStub.rpcStubs.snap_manageState
+      .withArgs('get')
+      .resolves({filecoin: {config: {derivationPath: "m/44'/461'/0'/0/0", network: "f"} as SnapConfig}});
+
+    walletStub.rpcStubs.snap_getBip44Entropy_461.resolves(testNewBip44Entropy);
+    walletStub.rpcStubs.web3_clientVersion.resolves(testNewMetamaskVersion);
+    // ensure our call to getBip44Entropy returns the correct entropy
+    walletStub.requestStub.resolves(testNewBip44Entropy);
+
     const result = await getKeyPair(walletStub);
     expect(result.publicKey).to.be.eq(testPublicKey);
     expect(result.address).to.be.eq(testAddress);
     expect(result.privateKey).to.be.eq(testPrivateKeyBase64);
     expect(walletStub.rpcStubs.snap_getBip44Entropy_461).to.have.been.calledOnce;
     expect(walletStub.rpcStubs.snap_manageState).to.have.been.calledOnce;
+    expect(walletStub.rpcStubs.web3_clientVersion).to.have.been.calledOnce;
   });
 });
--- packages/snap/test/unit/wallet.mock.test.ts
+++ packages/snap/test/unit/wallet.mock.test.ts
@@ -1,16 +1,19 @@
 import {Wallet} from "../../src/interfaces";
 import sinon from "sinon";
+import {testNewBip44Entropy, testNewMetamaskVersion} from "./rpc/keyPairTestConstants";
+import {SnapConfig} from "@chainsafe/filsnap-types";
 
 export class WalletMock implements Wallet {
   public readonly registerRpcMessageHandler = sinon.stub();
 
-  public readonly requestStub = sinon.stub()
+  public readonly requestStub = sinon.stub();
 
   public readonly rpcStubs = {
-    snap_manageState: sinon.stub(),
-    snap_getBip44Entropy_461: sinon.stub(),
     snap_confirm: sinon.stub(),
-  }
+    snap_getBip44Entropy_461: sinon.stub(),
+    snap_manageState: sinon.stub(),
+    web3_clientVersion: sinon.stub(),
+  };
 
   /**
    * Calls this.requestStub or this.rpcStubs[req.method], if the method has
@@ -19,9 +22,10 @@ export class WalletMock implements Wallet {
   public request(args: { method: string, params: unknown[] }): unknown {
     const { method, params = [] } = args;
     if (Object.hasOwnProperty.call(this.rpcStubs, method)) {
-      return (this.rpcStubs as any)[method](...params)
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      return (this.rpcStubs as any)[method](...params);
     }
-    return this.requestStub(args)
+    return this.requestStub(args);
   }
 
   public reset(): void {
@@ -30,5 +34,13 @@ export class WalletMock implements Wallet {
     Object.values(this.rpcStubs).forEach(
       (stub: ReturnType<typeof sinon.stub>) => stub.reset()
     );
+  }
+
+  public prepareFoKeyPair(): void {
+    this.rpcStubs.snap_manageState
+      .withArgs('get')
+      .resolves({filecoin: {config: {derivationPath: "m/44'/461'/0'/0/0", network: "f"} as SnapConfig}});
+    this.rpcStubs.snap_getBip44Entropy_461.resolves(testNewBip44Entropy);
+    this.rpcStubs.web3_clientVersion.resolves(testNewMetamaskVersion);
   }
 }
